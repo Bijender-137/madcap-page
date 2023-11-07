a0b38f2b840f425b4448acb5045b449cc1dc2ce4
Bijender-137 <bijender.k1996@gmail.com>
2023-11-07 14:31:03 +0530

    new code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./master.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import MyNave from "./components/MyNave";
+import Madcap from "./components/Madcap";
 
 function App() {
   return (
     <>
       <MyNave />
+      <Madcap/>
     </>
   );
 }
---
+++ src/components/Madcap.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Mynav from "./MyNave";
+
+const Madcap = () => {
+  return (
+    <>
+      <section>
+        <Container className="custom_container">{/* <Mynav/> */}</Container>
+      </section>
+    </>
+  );
+};
+
+export default Madcap;
