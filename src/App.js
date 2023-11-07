a13a662798e1b992b2ac5fcaf0e095fe3fa8d64e
Bijender-137 <bijender.k1996@gmail.com>
2023-11-07 14:13:40 +0530

    code merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import MyNave from './components/MyNave';
+import "./App.css";
+import "./about.css";
+import "./master.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import MyNave from "./components/MyNave";
 
 function App() {
   return (
     <>
-     <MyNave/>
+      <MyNave />
     </>
   );
 }
