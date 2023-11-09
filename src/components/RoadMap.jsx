623737777a1b35a7b133517ea94b116cf92e7ccc
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-09 14:04:37 +0530

    update

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -10,43 +10,45 @@ const RoadMap = () => {
     <>
       <Container className="custom_container px-0 position-relative ">
         <div className="yellow_border w-100"></div>
-        <div className="bg_clr_blue px-3">
-          <h1 className="text-white fs_2sm ff_MangaMaster fw-bold text-center mb-0 py-2">
-            road map
-          </h1>
-          <div className="madcap_1st_img m-auto">
-            <img
-              className="w-100 "
-              src={RoadMapFirstImg}
-              alt="RoadMapFirstImg"
-            />
+        <div className="hero_bg1 bg_clr_blue position-relative">
+          <div className="position-relative z-2 px-3">
+            <h1 className="text-white fs_2sm ff_MangaMaster fw-bold text-center mb-0 py-2">
+              road map
+            </h1>
+            <div className="madcap_1st_img m-auto">
+              <img
+                className="w-100 "
+                src={RoadMapFirstImg}
+                alt="RoadMapFirstImg"
+              />
+            </div>
+            <div className="m-auto madcap_1st_img mt-2">
+              <img
+                className="w-100"
+                src={RoadMapSecondImg}
+                alt="RoadMapSecondImg"
+              />
+            </div>
+            <div className="madcap_1st_img m-auto mt-3 pt-1">
+              <img
+                className="w-100"
+                src={RoadMapThirdImg}
+                alt="RoadMapThirdImg"
+              />
+            </div>
+            <div className="madcap_1st_img m-auto mt-3 pt-1">
+              <img
+                className="w-100"
+                src={RoadMapFourthImg}
+                alt="RoadMapFourthImg"
+              />
+            </div>
+            <p className="px-4 fw-bold ff_CrimeFighter fs_xsm text-white text-center pt-4 mt-2 mb-0 pb-2 lh_12">
+              This is a fictional comic representation. For full details, consult
+              our whitepaper. Investments come with market risks.
+            </p>
           </div>
-          <div className="m-auto madcap_1st_img mt-2">
-            <img
-              className="w-100"
-              src={RoadMapSecondImg}
-              alt="RoadMapSecondImg"
-            />
-          </div>
-          <div className="madcap_1st_img m-auto mt-3 pt-1">
-            <img
-              className="w-100"
-              src={RoadMapThirdImg}
-              alt="RoadMapThirdImg"
-            />
-          </div>
-          <div className="madcap_1st_img m-auto mt-3 pt-1">
-            <img
-              className="w-100"
-              src={RoadMapFourthImg}
-              alt="RoadMapFourthImg"
-            />
-          </div>
-          <p className="px-4 fw-bold ff_CrimeFighter fs_xsm text-white text-center pt-4 mt-2 mb-0 pb-2 lh_12">
-            This is a fictional comic representation. For full details, consult
-            our whitepaper. Investments come with market risks.
-          </p>
-        </div>
+       </div>
       </Container>
     </>
   );
