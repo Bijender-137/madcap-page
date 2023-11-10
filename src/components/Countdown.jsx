c3a43a2334c08218e57107406a1c2162180aa62a
Sandeep-Luhaniwal <sandeepluh0001.com>
2023-11-10 16:55:22 +0530

    errors slove and some mistak

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -38,16 +38,16 @@ const Countdown = () => {
             <div className="timeBox position-relative z-3 bg_lite_dark d-flex flex-column justify-content-center align-items-center px-1">
                 <div className="d-flex justify-content-center gap-2 px-md-4 mx-md-3 pt-3 mt-2">
                     <div className="timeContentBox bg-white d-flex justify-content-center align-items-center">
-                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center'>{days}<span className="d-block mb-0 color_white_400 ff_Samaritan">days</span></p>
+                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center fs_3sm'>{days}<span className="d-block mb-0 fs_xxs line-h-14 color_white_400 ff_Samaritan">days</span></p>
                     </div>
                     <div className="timeContentBox bg-white d-flex justify-content-center align-items-center">
-                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center'>{hours} <span className="d-block mb-0 color_white_400 ff_Samaritan">hours</span></p>
+                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center fs_3sm'>{hours} <span className="d-block mb-0 fs_xxs line-h-14 color_white_400 ff_Samaritan">hours</span></p>
                     </div>
                     <div className="timeContentBox bg-white d-flex justify-content-center align-items-center">
-                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center'>{minutes} <span className="d-block mb-0 color_white_400 ff_Samaritan">minutes</span></p>
+                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center fs_3sm'>{minutes} <span className="d-block mb-0 fs_xxs line-h-14 color_white_400 ff_Samaritan">minutes</span></p>
                     </div>
                     <div className="timeContentBox bg-white d-flex justify-content-center align-items-center">
-                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center'>{seconds} <span className="d-block mb-0 color_white_400 ff_Samaritan">seconds</span></p>
+                        <p className='color_dark_red ff_Badabb fw-normal mb-0 line-h-16 text-center fs_3sm'>{seconds} <span className="d-block mb-0 fs_xxs line-h-14 color_white_400 ff_Samaritan">seconds</span></p>
                     </div>
                 </div>
                 <p className='color_lite_white skew-0 ff_MangaMaster mb-0 pb-1 fw-bold letter_spacing_565 pt-2 mt-1 fs_2xs text-center lh-1'>48 hour flash pre-sale price: 0.0025
