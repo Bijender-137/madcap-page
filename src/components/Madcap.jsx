337e3ab7edaecd5619216ac20154e6755a8e6a91
Bijender-137 <bijender.k1996@gmail.com>
2023-11-10 14:33:04 +0530

    code update

--- src/components/Madcap.jsx
+++ src/components/Madcap.jsx
@@ -3,11 +3,9 @@ import { Container } from "react-bootstrap";
 import MadcapFirstImg from "../assets/images/webp/madcap_1st_img.webp";
 import MadcapSecondImg from "../assets/images/webp/madcap_2nd_img.webp";
 import MadcapThirdImg from "../assets/images/webp/madcap_3rd_img.webp";
-
 const Madcap = () => {
   return (
     <>
-
       <Container className="custom_container px-0">
         <div className="yellow_border w-100"></div>
         <div className="hero_bg1 position-relative bg_clr_blue">
@@ -34,7 +32,7 @@ const Madcap = () => {
               />
               <p className="fw-normal para position-absolute ff_CrimeFighter madcap_2nd_img_text mb-0">
                 Sure, I've tasted the dust, but I've never been afraid to get
-                back up. T hat's how you reach the stars!
+                back up. That's how you reach the stars!
               </p>
             </div>
             <div className="madcap_1st_img m-auto position-relative mt-2 mid_320">
@@ -45,7 +43,7 @@ const Madcap = () => {
               />
               <p className="fw-normal para position-absolute ff_CrimeFighter madcap_3rd_img_text mb-0">
                 Sure, I've tasted the dust, but I've never been afraid to get
-                back up. T hat's how you reach the stars!
+                back up. That's how you reach the stars!
               </p>
               <p className="fw-normal para position-absolute ff_CrimeFighter madcap_3rd_img_2nd_text mb-0">
                 Madcap Coin! Is not just a project, i t's a treasure map for
@@ -55,11 +53,11 @@ const Madcap = () => {
             <p className="fw-bold ff_CrimeFighter fs_xs text-white text-center pt-4 mt-2 pb-2 mb-0">
               This is a fictional comic representation. For full details,
               consult our whitepaper. Investments come with market risks.
-            </p>c
+            </p>
+            c
           </div>
         </div>
       </Container>
-
     </>
   );
 };
